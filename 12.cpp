272888e8ad6b5f4af83e404ce6384e8936dc1f30
wulu <wulu90@outlook.com>
2023-12-12 21:47:45 +0800

    day12 part2

--- 12.cpp
+++ 12.cpp
@@ -1,5 +1,7 @@
 #include <fstream>
 #include <iostream>
+#include <map>
+#include <queue>
 #include <sstream>
 #include <string>
 #include <vector>
@@ -9,7 +11,7 @@ struct row {
     string condition;
     vector<int> damagedgroup;
 
-    row(const string& str) {
+    row(const string& str, bool unfold = false) {
         auto space_index = str.find(' ');
         condition        = str.substr(0, space_index);
 
@@ -20,6 +22,18 @@ struct row {
             if (iss.peek() == ',') {
                 iss.ignore();
             }
+        }
+
+        if (unfold) {
+            auto tmpstr = condition;
+            auto tmpvec = damagedgroup;
+            for (int i = 0; i < 4; ++i) {
+                tmpstr.append(1, '?');
+                tmpstr.append(condition);
+                tmpvec.append_range(damagedgroup);
+            }
+            condition    = tmpstr;
+            damagedgroup = tmpvec;
         }
     }
 };
@@ -96,7 +110,168 @@ void part1() {
     cout << sum << endl;
 }
 
+struct pattern {
+    // string str;
+    int matched_groups;
+    int curr_damaged;
+};
+
+bool operator<(const pattern& lhs, const pattern& rhs) {
+    if (lhs.matched_groups == rhs.matched_groups) {
+        return lhs.curr_damaged < rhs.curr_damaged;
+    }
+    return lhs.matched_groups < rhs.matched_groups;
+}
+
+bool check(const char& c, const vector<int>& v, pattern& p) {
+    if (p.matched_groups == v.size()) {
+        if (c == '.') {
+            // p.str += c;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    bool res = true;
+    if (c == '#') {
+        if (p.curr_damaged + 1 > v[p.matched_groups]) {
+            res = false;
+        } else {
+            // p.str += c;
+            ++p.curr_damaged;
+        }
+    } else {
+        if (p.curr_damaged > 0) {
+            if (p.curr_damaged < v[p.matched_groups]) {
+                res = false;
+            } else {
+                // p.str += c;
+                p.curr_damaged = 0;
+                ++p.matched_groups;
+            }
+        } else {
+            // p.str += c;
+        }
+    }
+    return res;
+}
+
+int64_t arrangements(const string& str, const vector<int>& v) {
+    // queue<pattern> qs;
+    // qs.push({});
+
+    // for (int i = 0; i < str.size(); ++i) {
+    //     size_t num = qs.size();
+    //     for (int j = 0; j < num; ++j) {
+    //         auto p = qs.front();
+
+    //         if (str[i] != '?') {
+    //             auto tmp = p;
+    //             if (check(str[i], v, tmp) &&
+    //                 (i < str.size() - 1 ||
+    //                  (i == str.size() - 1 &&
+    //                   (tmp.matched_groups == v.size() || (tmp.matched_groups == v.size() - 1 && tmp.curr_damaged == *v.rbegin()))))) {
+    //                 qs.push(tmp);
+    //             }
+    //         } else {
+    //             auto t1 = p;
+    //             auto t2 = p;
+    //             if (check('.', v, t1) &&
+    //                 (i < str.size() - 1 || (i == str.size() - 1 && (t1.matched_groups == v.size() || (t1.matched_groups == v.size() - 1
+    //                 &&
+    //                                                                                                   t1.curr_damaged == *v.rbegin())))))
+    //                                                                                                   {
+    //                 qs.push(t1);
+    //             }
+    //             if (check('#', v, t2) &&
+    //                 (i < str.size() - 1 || (i == str.size() - 1 && (t2.matched_groups == v.size() || (t2.matched_groups == v.size() - 1
+    //                 &&
+    //                                                                                                   t2.curr_damaged == *v.rbegin())))))
+    //                                                                                                   {
+    //                 qs.push(t2);
+    //             }
+    //         }
+    //         qs.pop();
+    //     }
+    // }
+
+    map<pattern, int64_t> pattern_size;
+    pattern_size.insert({{}, 1});
+
+    for (int i = 0; i < str.size(); ++i) {
+        // size_t num=ps.size();
+        map<pattern, int64_t> tmp_ps;
+
+        for (auto [p, s] : pattern_size) {
+            if (str[i] != '?') {
+                auto tmp = p;
+                if (check(str[i], v, tmp)) {
+                    if (i < str.size() - 1) {
+                        tmp_ps[tmp] += s;
+                    }
+                    if (i == str.size() - 1) {
+                        if (tmp.matched_groups == v.size() || (tmp.matched_groups == v.size() - 1 && tmp.curr_damaged == *v.rbegin())) {
+                            tmp_ps[tmp] += s;
+                        }
+                    }
+                }
+            } else {
+                auto t1 = p;
+                auto t2 = p;
+                if (check('.', v, t1)) {
+                    if (i < str.size() - 1) {
+                        tmp_ps[t1] += s;
+                    }
+                    if (i == str.size() - 1) {
+                        if (t1.matched_groups == v.size() || (t1.matched_groups == v.size() - 1 && t1.curr_damaged == *v.rbegin())) {
+                            tmp_ps[t1] += s;
+                        }
+                    }
+                }
+                if (check('#', v, t2)) {
+                    if (i < str.size() - 1) {
+                        tmp_ps[t2] += s;
+                    }
+                    if (i == str.size() - 1) {
+                        if (t2.matched_groups == v.size() || (t2.matched_groups == v.size() - 1 && t2.curr_damaged == *v.rbegin())) {
+                            tmp_ps[t2] += s;
+                        }
+                    }
+                }
+            }
+        }
+        pattern_size = tmp_ps;
+    }
+
+    int64_t sum = 0;
+    for (auto [p, s] : pattern_size) {
+        sum += s;
+    }
+    return sum;
+}
+
+void part2() {
+    ifstream input("input");
+    string str;
+    vector<row> rows;
+    while (getline(input, str)) {
+        rows.emplace_back(str, true);
+    }
+
+    int64_t sum = 0;
+    int i       = 1;
+    for (auto row : rows) {
+        sum += arrangements(row.condition, row.damagedgroup);
+        cout << i << "/" << rows.size() << " " << sum << endl;
+        ++i;
+    }
+
+    cout << sum << endl;
+}
+
 int main() {
     part1();
+    part2();
     return 0;
 }
