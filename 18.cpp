4ed10c4295a6f5d5fc9ef4c03e564e9b873f4502
wulu <wulu90@outlook.com>
2023-12-18 14:23:55 +0800

    day18 part2

--- 18.cpp
+++ 18.cpp
@@ -5,13 +5,29 @@
 
 using namespace std;
 
+enum class direction { R, D, L, U };
+vector<vector<int>> dirsum{{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+
 struct node {
-    char direction;
+    direction dire;
     int32_t meters;
     int32_t color;
 
     node(const string& str) {
-        direction      = str[0];
+        switch (str[0]) {
+        case 'R':
+            dire = direction::R;
+            break;
+        case 'D':
+            dire = direction::D;
+            break;
+        case 'L':
+            dire = direction::L;
+            break;
+        default:
+            dire = direction::U;
+            break;
+        }
         auto lastspace = str.find(' ', 2);
         meters         = stoi(str.substr(2, lastspace - 2));
         std::size_t pos{};
@@ -28,27 +44,12 @@ void part1() {
 
     vector<pair<int, int>> coords{{0, 0}};
 
+    int point_boundary = 0;
     for (auto [d, m, c] : plan) {
         auto [x, y] = *coords.rbegin();
-        {
-            if (d == 'R') {
-                for (int i = 1; i <= m; ++i) {
-                    coords.push_back({x, y + i});
-                }
-            } else if (d == 'D') {
-                for (int i = 1; i <= m; ++i) {
-                    coords.push_back({x + i, y});
-                }
-            } else if (d == 'L') {
-                for (int i = 1; i <= m; ++i) {
-                    coords.push_back({x, y - i});
-                }
-            } else {
-                for (int i = 1; i <= m; ++i) {
-                    coords.push_back({x - i, y});
-                }
-            }
-        }
+        coords.push_back({x + dirsum[int(d)][0] * m, y + dirsum[int(d)][1] * m});
+
+        point_boundary += m;
     }
 
     int area = 0;
@@ -56,10 +57,47 @@ void part1() {
         area += coords[i].first * coords[i + 1].second - coords[i].second * coords[i + 1].first;
     }
 
-    cout << abs(area) / 2 + 1 - (coords.size() - 1) / 2 + coords.size() - 1 << endl;
+    cout << abs(area) / 2 + 1 + (point_boundary) / 2 << endl;
+}
+
+struct node2 {
+    direction d;
+    int64_t m;
+
+    node2(const string& str) {
+        auto lastspace = str.find(' ', 2);
+        std::size_t pos{};
+        m = stoi(str.substr(lastspace + 3, 5), &pos, 16);
+        d = direction{str[str.size() - 2] - '0'};
+    }
+};
+
+void part2() {
+    ifstream input("input");
+    vector<node2> plan;
+    for (string str; getline(input, str);) {
+        plan.emplace_back(str);
+    }
+
+    vector<pair<int64_t, int64_t>> coords{{0, 0}};
+
+    int64_t point_boundary = 0;
+    for (auto [d, m] : plan) {
+        auto [x, y] = *coords.rbegin();
+        coords.push_back({x + dirsum[int(d)][0] * m, y + dirsum[int(d)][1] * m});
+        point_boundary += m;
+    }
+
+    int64_t area = 0;
+    for (size_t i = 0; i < coords.size() - 1; ++i) {
+        area += coords[i].first * coords[i + 1].second - coords[i].second * coords[i + 1].first;
+    }
+
+    cout << abs(area) / 2 + 1 + (point_boundary) / 2 << endl;
 }
 
 int main() {
     part1();
+    part2();
     return 0;
 }
